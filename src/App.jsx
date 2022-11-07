df842a82077af6fb983b8f1a0ecaa045714f667d
indiflex <indiflex1@gmail.com>
2022-11-07 14:20:40 +0900

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from 'react';
+import { List } from './components/Mark';
 import { Nav } from './components/Nav';
 
+const SampleData = {
+  books: [
+    { id: 1, title: 'Private Book' },
+    { id: 2, title: 'React Study' },
+  ],
+};
+
 function App() {
+  const [data, setData] = useState(SampleData);
+
   return (
     <div>
-      <Nav />
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <List />
+      </main>
+      {/* <footer>Copyright Indiflex Senior Coding</footer> */}
     </div>
   );
 }
