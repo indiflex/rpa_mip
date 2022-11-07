6b224a8ca0b553825e878767a29aeec9dcbf21d7
indiflex <indiflex1@gmail.com>
2022-11-07 14:10:05 +0900

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+// import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
+import { BookmarkSquareIcon } from '@heroicons/react/24/outline';
+
+export const Nav = () => {
+  return (
+    <nav className='flex items-center justify-between px-2 shadow'>
+      <div>
+        <h1 className='text-2xl font-bold flex'>
+          <BookmarkSquareIcon className='w-8 text-gray-500' /> Sico Index
+        </h1>
+      </div>
+      <div>
+        {/* <MagnifyingGlassCircleIcon className='w-4 absolute h-8' /> */}
+        <input type='text' placeholder='search...' className='w-24 h-10' />
+      </div>
+    </nav>
+  );
+};
